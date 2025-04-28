9eb662b71b1ccaa437c4d58fc07f7245b3c6ddb9
Siddhesh <sidd.mhapankar7@gmail.com>
2025-04-28 23:48:20 +0530

    added vercel.json for deploying in vercel and changes in mongoose connection

--- server.js
+++ server.js
@@ -10,11 +10,42 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI, {
     dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Db connected"))
   .catch((err) => console.error(err));
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am live");
+});
+
+app.use((req, res, next) => {
+  const allowedOrigins = ["http://localhost:3000"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With, Accept, Origin"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
