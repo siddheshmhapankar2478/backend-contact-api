40d7344d9f820372b52fb6d31d79a4fa50a096ca
Siddhesh <sidd.mhapankar7@gmail.com>
2025-05-01 16:18:16 +0530

    allowing session_token in header

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With, Accept, Origin"
+    "Content-Type, Authorization, X-Requested-With, Accept, Origin, session-token"
   );
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
